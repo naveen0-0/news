72973cdf501973b8cca7dd02ee9c331b151f3443
Naveen <naveen.k@mtechzilla.com>
2023-05-04 07:22:47 +0530

    fix: updated styles

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,18 +1,15 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { navbarLinks } from "../utils/constants";
 
 export default function Links() {
-   return (
-      <div className="links">
-         <div className="linkDiv"><Link to="/" className="link">HeadLines</Link></div>
-         <div className="linkDiv"><Link to="/business" className="link">Business</Link></div>
-         <div className="linkDiv"><Link to="/entertainment" className="link">Entertainment</Link></div>
-         <div className="linkDiv"><Link to="/politics" className="link">Politics</Link></div>
-         <div className="linkDiv"><Link to="/tech" className="link">Technology</Link></div>
-         <div className="linkDiv"><Link to="/sports" className="link">Sports</Link></div>
-         <div className="linkDiv"><Link to="/lifestyle" className="link">LifeStyle</Link></div>
-         <div className="linkDiv"><Link to="/india" className="link">India</Link></div>
-         <div className="linkDiv"><Link to="/world" className="link">World</Link></div>
-      </div>
-   )
+  return (
+    <div className="links">
+      {navbarLinks.map((link) => (
+        <Link to={link.link} key={link.id} className="link">
+          {link.text}
+        </Link>
+      ))}
+    </div>
+  );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const navbarLinks = [
+  { id: 1, text: "Headlines", link: "/" },
+  { id: 2, text: "Business", link: "/business" },
+  { id: 3, text: "Entertainment", link: "/entertainment" },
+  { id: 4, text: "Politics", link: "/politics" },
+  { id: 5, text: "Technology", link: "/tech" },
+  { id: 6, text: "Sports", link: "/sports" },
+  { id: 7, text: "LifeStyle", link: "/lifestyle" },
+  { id: 8, text: "LifeStyle", link: "/lifestyle" },
+  { id: 9, text: "World", link: "/world" },
+];
